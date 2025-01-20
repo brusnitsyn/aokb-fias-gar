c16b597935ee84e70fcc8bc367f141e3601b7ff6
brusnitsyn <andrew.brusnitsyn@gmail.com>
2025-01-20 11:19:32 +0900

    update index route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $content = file_get_contents('../composer.json');
+    $content = json_decode($content,true);
+    $host = \request()->getSchemeAndHttpHost();
+
+    return [
+        'name' => $content['name'],
+        'description' => $content['description'],
+        'version' => $content['version'],
+        'api' => "$host/api"
+    ];
 });
